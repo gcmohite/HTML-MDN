d2b709acfb2443a85819e6facd393b491a8cf898
Gautam Mohite <gautam.mohite1005@gmail.com>
2023-06-30 09:36:54 +0530

    completed Advanced Form Styling

---
+++ 04_HTML Forms/06_Advanced_Form_Styling/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const toggleSwitch = document.querySelector('#toggle');
+const switchStatus = document.querySelector('label[for="toggle"]');
+
+toggleSwitch.addEventListener('click', () => {
+  toggleSwitch.checked
+    ? (switchStatus.textContent = 'ON')
+    : (switchStatus.textContent = 'OFF');
+});
+
+const slider = document.querySelector('#slider');
+const sliderReading = document.querySelector('output');
+
+slider.addEventListener('input', function () {
+  // console.log(this);
+  sliderReading.textContent = this.value;
+
+  if (this.value > 80) {
+    slider.style.backgroundColor = 'red';
+  } else if (this.value <= 60) {
+    this.style.backgroundColor = 'green';
+  } else if (this.value > 60 && this.value <= 80) {
+    this.style.backgroundColor = 'darkgoldenrod';
+  } else {
+    this.style.backgroundColor = 'green';
+  }
+});
+console.log(slider.value);
